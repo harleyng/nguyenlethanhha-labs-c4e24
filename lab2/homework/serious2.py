a9880acddd253da8e49bc1ba5ce803f8c57d1bec
Thanh Ha Ng Le <harley.ngx@gmail.com>
2018-12-21 11:35:24 +0700

    initial commit

--- lab2/homework/serious2.py
+++ lab2/homework/serious2.py
@@ -11,44 +11,50 @@ raw_data = conn.read()
 page_content = raw_data.decode("utf8")
 soup = BeautifulSoup(page_content, "html.parser")
 
-tieu_de_list = []
+
 
 # import xlsxwriter
 # workbook  = xlsxwriter.Workbook('doanhthu.xlsx')
 # worksheet = workbook.add_worksheet()
+doanh_thu_list=[]
 
 # Title
 head = soup.find("table", id="tblGridData")
-tr = head.tr
-td_truoc_sau = tr.find("td", "b_r_c")
+trs = head.tr
+td_truoc_sau = trs.find("td", "b_r_c")
 div = td_truoc_sau.div
 a_list = div.find_all("a")
 
-
 for a in a_list:
     img = a.img 
-    
     mui_ten = img["src"]
     truoc_sau = a.string
-    print(mui_ten)
-
+    # chuyen_doi = mui_ten + truoc_sau
     # image_data = BytesIO(urlopen(mui_ten).read())
     # worksheet.insert_image('A1', mui_ten, {'image_data': image_data})
 
-    tieu_de_list.append(truoc_sau)
-    # tieu_de_list.append(mui_ten)
-
-td_quy_list = tr.find_all("td", "h_t")
+td_quy_list = trs.find_all("td", "h_t")
+tieu_de_list = []
 for td_quy in td_quy_list:
     quy = td_quy.string
-    # tieu_de_list.append(quy)
+    print(quy)
+    tieu_de_list.append(quy)
 
-td_tang_truong = tr.find("td", "h_t_tt")
-tang_truong = td_tang_truong.string
-# tieu_de_list.append(td_tang_truong)
+# Body
+body = soup.find("table", id="tableContent")
+tr_list = body.find_all("tr")
+for tr in tr_list:
+    td_list = tr.find_all("td", align = "right")
+    for td in td_list:
+        content = td.string
+        for tieu_de in tieu_de_list:
+            
+            doanh_thu = OrderedDict({
+                tieu_de: content
+            })
+            # print(tieu_de)
+            # print(content)
+            doanh_thu_list.append(doanh_thu)
 
-
-# body = soup.find("table", id="tableContent")
-
-pyexcel.save_as(records=tieu_de_list, dest_file_name="doanhthu.xlsx") 
+# pyexcel.save_as(records=doanh_thu_list, dest_file_name="doanhthu.xlsx") 
 
